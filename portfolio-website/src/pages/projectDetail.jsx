f4966a006cb86f0475bc73d50720f9378a14469e
Tamilmughilan <138361053+Tamilmughilan@users.noreply.github.com>
2025-01-28 07:50:55 +0000

    Screenshot updated

--- portfolio-website/src/pages/projectDetail.jsx
+++ portfolio-website/src/pages/projectDetail.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import './ProjectDetail.css';
 
-
 const projects = [
   { 
     id: 1,
@@ -28,17 +27,16 @@ Google API Client: This project uses Google’s API client to interact with the 
     id: 3,
     name: 'Django Chat', 
     link: 'https://github.com/Tamilmughilan/Django_chat',
-    description: `A simple and feature-rich chat application built using Django, featuring user authentication, CRUD operations, WebSockets, and real-time messaging.\n
+    description: `A simple and feature-rich chat application built using Django, featuring user authentication, CRUD operations, WebSockets, and real-time messaging.
 Features:
--CRUD operations for users (Create, Read, Update, Delete)
--Create and join chat rooms, and engage in group chats
--User-friendly, responsive UI
--WebSocket integration for real-time communication
--Authentication for WebSockets to prevent unauthorized access
--Profile management with username and profile picture
--Persistent message storage in the database
-`,
-    images: ['/assets/django1.jpg','/assets/django2.jpg','/assets/django3.jpg','/assets/django4.jpg'],
+- CRUD operations for users (Create, Read, Update, Delete)
+- Create and join chat rooms, and engage in group chats
+- User-friendly, responsive UI
+- WebSocket integration for real-time communication
+- Authentication for WebSockets to prevent unauthorized access
+- Profile management with username and profile picture
+- Persistent message storage in the database`,
+    images: ['/assets/django1.jpg', '/assets/django2.jpg', '/assets/django3.jpg', '/assets/django4.jpg'],
     screenRecording: '',
   },
   { 
@@ -49,12 +47,11 @@ Features:
     images: [],
     screenRecording: '',
   },
-  
   { 
     id: 4,
     name: 'Parse-X', 
     link: 'https://github.com/Tamilmughilan/Parse-X',
-    description: `Parse-X is a Resume and JD (Job Description) comparison software with advanced features designed to streamline the hiring process. It evaluates resumes against job descriptions and calculates the ATS (Applicant Tracking System) score to identify the best candidates. 
+    description: `Parse-X is a Resume and JD (Job Description) comparison software with advanced features designed to streamline the hiring process. It evaluates resumes against job descriptions and calculates the ATS (Applicant Tracking System) score to identify the best candidates.
 
 Key Features:
 - Resume and JD Evaluation    : Compares resumes with job descriptions to find the best match based on keywords, skills, and experience.
@@ -64,7 +61,7 @@ Key Features:
 - Candidate Sorting          : Automatically sorts and filters candidates based on resume-JD compatibility and ATS scores, providing a ranked list of suitable candidates.
 
 This project simplifies recruitment by combining automation, efficiency, and accurate evaluation.`,
-    images: ['/assets/parse1.png','/assets/parse2.png','/assets/parse3.png','/assets/parse4.png'],
+    images: ['/assets/parse1.png', '/assets/parse2.png', '/assets/parse3.png', '/assets/parse4.png'],
     screenRecording: '',
   },
   { 
@@ -72,22 +69,21 @@ This project simplifies recruitment by combining automation, efficiency, and acc
     name: 'Quiz app', 
     link: 'https://github.com/Tamilmughilan/Parse-X',
     description: `Flutter Quiz App
-This is a simple Flutter Quiz App where users can take a quiz with multiple choice questions. The app features displaying the user's score after completing the quiz. It has a dark themed look which is not harsh for the users eyes.
+This is a simple Flutter Quiz App where users can take a quiz with multiple choice questions. The app features displaying the user's score after completing the quiz. It has a dark themed look which is not harsh for the user's eyes.
 
 Features
--Start Quiz
--Multiple-choice questions with options
--Display the total score upon quiz completion
--Intuitive UI design
+- Start Quiz
+- Multiple-choice questions with options
+- Display the total score upon quiz completion
+- Intuitive UI design
 
 Technologies Used
--Flutter
--Dart
--Provider for state management`,
-    images: ['/assets/quiz1.jpg','/assets/quiz2.jpg','/assets/quiz3.jpg'],
+- Flutter
+- Dart
+- Provider for state management`,
+    images: ['/assets/quiz1.jpg', '/assets/quiz2.jpg', '/assets/quiz3.jpg'],
     screenRecording: '/assets/quiz_app.mp4',
   },
-  
 ];
 
 function ProjectDetail() {
@@ -100,31 +96,30 @@ function ProjectDetail() {
 
   return (
     <div className="project-detail-container">
-  <div className="project-detail">
-    <h2>{project.name}</h2>
-    <p>{project.description}</p>
-    <a href={project.link} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      <div className="project-detail">
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <a href={project.link} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
 
-    <div className="screenshots">
-      {project.images.length > 0 ? (
-        project.images.map((img, index) => (
-          <img key={index} src={img} alt={`Screenshot ${index + 1}`} />
-        ))
-      ) : (
-        <p className="no-data">No screenshots available</p>
-      )}
+        <div className="screenshots">
+          {project.images.length > 0 ? (
+            project.images.map((img, index) => (
+              <img key={index} src={process.env.PUBLIC_URL + img} alt={`Screenshot ${index + 1}`} />
+            ))
+          ) : (
+            <p className="no-data">No screenshots available</p>
+          )}
+        </div>
+
+        {project.screenRecording ? (
+          <video controls>
+            <source src={process.env.PUBLIC_URL + project.screenRecording} type="video/mp4" />
+          </video>
+        ) : (
+          <p className="no-data">No screen recording available</p>
+        )}
+      </div>
     </div>
-
-    {project.screenRecording ? (
-      <video controls>
-        <source src={project.screenRecording} type="video/mp4" />
-      </video>
-    ) : (
-      <p className="no-data">No screen recording available</p>
-    )}
-  </div>
-</div>
-
   );
 }
 
