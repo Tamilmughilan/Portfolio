420a6187b8d60f77e67ba75c3ae0779e9d6f0349
Tamilmughilan <138361053+Tamilmughilan@users.noreply.github.com>
2025-01-28 07:44:30 +0000

    Update base path in vite.config.js for GitHub Pages

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/portfolio-website/', // Adjust for nested folder
+  base: '/Portfolio/', // Adjust for nested folder
 });
