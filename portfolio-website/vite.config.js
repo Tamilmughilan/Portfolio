684ef2f6e901d7848957e3c2286b5a90a633f468
TAMIL MUGHILAN E <138361053+Tamilmughilan@users.noreply.github.com>
2025-03-14 05:11:22 +0000

    routing fix

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: '/', // Change this to match your GitHub Pages setup
   build: {
-    outDir: 'dist',
-    // Generate a 404.html file that redirects to index.html
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      }
-    }
+    outDir: 'dist'
   }
 });
